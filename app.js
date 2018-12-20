07c48dbe39577582fe456bc8bedf1a07051e41ca
Jackie <jacgut19@gmail.com>
2018-12-20 15:38:56 +0000

    adding html elements using append +=

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ Array.from(books).forEach(function(book){
 // passing in a function which is going to fire on each book
 // (its gonna take the individual book and iterate each one)
     
-    console.log(book.textContent);
-    // textContent - this property is going to grab the text within the book element 
+    book.textContent += '(book title)';
+    // append(add) more text with the span element text
 });
+
+
+const bookList = document.querySelector('#book-list');
+//console.log(bookList.innerHTML);
+
+//bookList.innerHTML = '<h2>Books and more books...</h2>';
+// this will replace all that innner HTML with the h2 element
+
+bookList.innerHTML += '<p>This is how you add HTML element.</>';
+// this will append(add on to) the html by creating a <p> tag
+
+
+
