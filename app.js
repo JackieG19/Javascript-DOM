1f3de98d62142aa74d7fa15b9fdd4a12b6f66f6e
Jackie <jacgut19@gmail.com>
2018-12-20 15:16:27 +0000

    Changing Text & HTML Content

--- app.js
+++ app.js
@@ -1,33 +1,12 @@
-const wrap = document.querySelector('#wrapper');
-// constant set it = to this method is going to go out 
-// and grab this wrapper Id element and return it
+var books = document.querySelectorAll('#book-list li .name');
+// this will grab al of the span tag with class of name and store in the variable books 
 
-console.log(wrap); 
-// this should print out the html element that has the Id of wrapper
-
-const wmf = document.querySelector('#book-list li:nth-child(2).name');
-// constant set it = to this method that grabs the second child of the Li element by using 
-// a pseudo class :nth-child in (put the second child of LI element).then class of name
-
-console.log(wmf);
-// this should print out the the book in the list element with the Id of book-list
-
-const book = document.querySelector('#book-list li .name');
-// the querySelector method only returns one element 
-console.log(book);
-//will print out the first element with the classname of book-list 
-
-const books = document.querySelectorAll('#book-list li .name');
-// the querySelectorAll method will return a collection of element 
-// so this will grab every element with an element class of name inside and li inside the book-list
-console.log(books);
-// this print out all the elements  with the classname of name
-
-// this is how you cycle through the collection of books into an array 
 Array.from(books).forEach(function(book){
-// creating an array using the Array.from method (from the variable books)
-//turning the book collections into an array using the forEach() method
-// passing through a function(the book iterate through at that moment)
-
-    console.log(book);
+// turning the book collection into an array
+// using the forEach method to cycle through them 
+// passing in a function which is going to fire on each book
+// (its gonna take the individual book and iterate each one)
+    
+    console.log(book.textContent);
+    // textContent - this property is going to grab the text within the book element 
 });
